0ae48b61ca3bc913fb4067402da863e30956cd2c
Dima Krasner <dima@dimakrasner.com>
2017-07-26 19:52:56 +0300

    Added tests for spawn

---
+++ tests/b6b_test_spawn.c
@@ -0,0 +1,79 @@
+/*
+ * This file is part of b6b.
+ *
+ * Copyright 2017 Dima Krasner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#include <b6b.h>
+
+int main()
+{
+	struct b6b_interp interp;
+
+	assert(b6b_interp_new_argv(&interp, 0, NULL, B6B_OPT_TRACE));
+	assert(b6b_call_copy(
+	                &interp,
+	                "{$global x {}} " \
+	                "{$spawn {{$map y [$range 1 20] {{$list.append $x a}}}}} " \
+	                "{$map y [$range 1 20] {{$list.append $x b}}} " \
+	                "{$return $x} " ,
+	                15 + 56 + 45 + 13) == B6B_RET);
+	assert(b6b_as_str(interp.fg->_));
+	assert(strstr(interp.fg->_->s, "a a b b"));
+	assert(strstr(interp.fg->_->s, "b b a a"));
+	b6b_interp_destroy(&interp);
+
+	assert(b6b_interp_new_argv(&interp, 0, NULL, B6B_OPT_TRACE));
+	assert(b6b_call_copy(&interp,
+	                "{$global x {}} " \
+	                "{$spawn {{$map y [$range 1 20] {{$list.append $x a}}}}} " \
+	                "{$spawn {{$map y [$range 1 20] {{$list.append $x b}}}}} " \
+	                "{$map y [$range 1 20] {{$list.append $x c}}} " \
+	                "{$return $x} " ,
+	                15 + 56 + 56 + 45 + 13) == B6B_RET);
+	assert(b6b_as_str(interp.fg->_));
+	assert(strstr(interp.fg->_->s, "a a b b") ||
+	       strstr(interp.fg->_->s, "a a c c"));
+	assert(strstr(interp.fg->_->s, "b b a a") ||
+	       strstr(interp.fg->_->s, "b b c c"));
+	assert(strstr(interp.fg->_->s, "c c a a") ||
+	       strstr(interp.fg->_->s, "c c b b"));
+	b6b_interp_destroy(&interp);
+
+	assert(b6b_interp_new_argv(&interp, 0, NULL, B6B_OPT_TRACE));
+	assert(b6b_call_copy(&interp,
+	        "{$global x {}} " \
+	        "{$spawn {{$map y [$range 1 3] {{$list.append $x a} {$yield}}}}} " \
+	        "{$map y [$range 1 3] {{$list.append $x b} {$yield}}} " \
+	        "{$return $x} " ,
+	        15 + 64 + 53 + 13) == B6B_RET);
+	assert(b6b_as_str(interp.fg->_));
+	assert(strcmp(interp.fg->_->s, "b a b a b a") == 0);
+	b6b_interp_destroy(&interp);
+
+	assert(b6b_interp_new_argv(&interp, 0, NULL, B6B_OPT_TRACE));
+	assert(b6b_call_copy(&interp,
+	                     "{$global x {}} " \
+	                     "{$spawn {{$yield} {$exit 7}}} " \
+	                     "{$while 1 {{$yield}}}",
+	                     15 + 30 + 21) == B6B_EXIT);
+	b6b_interp_destroy(&interp);
+
+	return EXIT_SUCCESS;
+}
