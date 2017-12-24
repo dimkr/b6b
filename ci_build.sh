b3c52db981719faf1dd17d3d654836d1036b34cf
Dima Krasner <dima@dimakrasner.com>
2017-12-24 08:25:45 +0200

    Moved coverage reporting to the container, again

--- ci_build.sh
+++ ci_build.sh
@@ -16,34 +16,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# we want coverage data to point to source files at identical paths, inside and outside of the container
-ln -s `pwd` /tmp/b6b
-cd /tmp/b6b
-
 export CFLAGS=-g
-meson -Dwith_valgrind=true /builds/gcc
-CC=clang meson -Dwith_valgrind=true /builds/clang
-meson -Dwith_threads=false -Dwith_valgrind=true /builds/no-threads
-meson -Dwith_threads=false -Dwith_miniz=false -Dwith_linenoise=false /builds/small
-meson -Db_coverage=true -Doptimistic_alloc=false /builds/coverage
+meson -Dwith_valgrind=true build-gcc
+CC=clang meson -Dwith_valgrind=true build-clang
+meson -Dwith_threads=false -Dwith_valgrind=true build-no-threads
+meson -Dwith_threads=false -Dwith_miniz=false -Dwith_linenoise=false build-small
+meson -Db_coverage=true -Doptimistic_alloc=false build-coverage
 
 # build with GCC, clang, with GCC while thread support is disabled and a small build with all optional features off
-for i in /builds/gcc /builds/clang /builds/no-threads
+for i in build-gcc build-clang build-no-threads
 do
 	ninja -C $i
 	meson configure -Dbuildtype=release $i
 	DESTDIR=dest ninja -C $i install
 done
 
-DESTDIR=dest ninja -C /builds/small install
-meson test -C /builds/small --no-rebuild --print-errorlogs
+DESTDIR=dest ninja -C build-small install
+meson test -C build-small --no-rebuild --print-errorlogs
 
 # rebuild the small build with a static library
-meson configure -Ddefault_library=static /builds/small
-DESTDIR=dest-static ninja -C /builds/small install
+meson configure -Ddefault_library=static build-small
+DESTDIR=dest-static ninja -C build-small install
 
 # run all tests
-for i in /builds/gcc /builds/clang
+for i in build-gcc build-clang
 do
 	meson test -C $i --no-rebuild --print-errorlogs --repeat 5
 	meson test -C $i --no-rebuild --print-errorlogs --wrapper "taskset -c 0"
@@ -53,11 +49,13 @@ done
 export LD_BIND_NOW=1
 
 # run all tests, with Valgrind
-for i in /builds/gcc /builds/clang
+for i in build-gcc build-clang
 do
 	meson test -C $i --no-rebuild --print-errorlogs --no-suite=b6b:slow --num-processes 1 -t 2 --wrapper "valgrind --leak-check=full --error-exitcode=1 --malloc-fill=1 --free-fill=1 --track-fds=yes"
 	meson test -C $i --no-rebuild --print-errorlogs --no-suite=b6b:slow --num-processes 1 -t 2 --wrapper "valgrind --tool=helgrind --error-exitcode=1"
 	meson test -C $i --no-rebuild --print-errorlogs --no-suite=b6b:slow --num-processes 1 -t 2 --wrapper "valgrind --tool=helgrind --error-exitcode=1 --fair-sched=yes"
 done
 
-meson test -C /builds/coverage
+meson test -C build-coverage
+cd build-coverage
+curl -s https://codecov.io/bash | bash
