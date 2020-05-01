b81a1f1a5c6bf0d4a72ce38f661b0b3fb8fc5fc7
Dima Krasner <dima@dimakrasner.com>
2020-05-01 15:25:58 +0300

    Revert "Remove -pedantic"
    This reverts commit 6c89cdbf76d58b86babf0621d8f95b336d0c5a56.

--- ci_build.sh
+++ ci_build.sh
@@ -18,11 +18,11 @@
 
 apt-get -y --no-install-recommends install libffi-dev curl
 
-meson --warnlevel=2 -Dwith_valgrind=true build-gcc
-CC="ccache clang" meson --warnlevel=2 -Dwith_valgrind=true build-clang
-meson --warnlevel=2 -Dwith_threads=false -Dwith_valgrind=true build-no-threads
-meson --warnlevel=2 -Dwith_threads=false -Dwith_miniz=false -Dwith_linenoise=false build-small
-meson --warnlevel=2 --optimization=3 -Db_sanitize=address build-asan
+meson --warnlevel=3 -Dwith_valgrind=true build-gcc
+CC="ccache clang" meson --warnlevel=3 -Dwith_valgrind=true build-clang
+meson --warnlevel=3 -Dwith_threads=false -Dwith_valgrind=true build-no-threads
+meson --warnlevel=3 -Dwith_threads=false -Dwith_miniz=false -Dwith_linenoise=false build-small
+meson --warnlevel=3 --optimization=3 -Db_sanitize=address build-asan
 
 # build with GCC, clang, with GCC while thread support is disabled and a small build with all optional features off
 for i in build-gcc build-clang build-no-threads
